425ff6d722bd81b5a9825a77fcbacc3c1a1aade7
Fortino Sandoval <fortysc97@gmail.com>
2020-12-23 09:14:15 -0800

    reducer, actions, model and state

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,16 @@
+import { createReducer, on } from '@ngrx/store';
+import { Todo } from './models/todo.model';
+import { create } from './todo.actions';
+
+export const initialState: Todo[] = [
+  new Todo('salvar al mundo')
+];
+
+const _todoReducer = createReducer(
+  initialState,
+  on(create, (state, { text }) => [...state, new Todo(text)]),
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
